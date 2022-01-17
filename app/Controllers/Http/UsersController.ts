503d692fe695d3f8347b04b3b85d6d5c169b0faf
João Gabriel <main.joaogabrielsg@gmail.com>
2022-01-17 11:13:23 -0300

    feat: add authentication when user register

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -5,11 +5,24 @@ import CreateUserValidator from 'App/Validators/CreateUserValidator'
 export default class UsersController {
   public async index({}: HttpContextContract) {}
 
-  public async store({ request, response }: HttpContextContract) {
+  public async store({ auth, request }: HttpContextContract) {
     const newUser = await request.validate(CreateUserValidator)
-    await User.create(newUser)
+    const user = await User.create(newUser)
+    const token = await auth.use('api').attempt(newUser.email, newUser.password, {
+      expiresIn: '30mins',
+      name: newUser.email,
+    })
 
-    return response.status(201).json({ message: 'Created user.' })
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      created_at: user.createdAt,
+      updated_at: user.updatedAt,
+    }
+
+    return { token, user: userWithoutPassword }
   }
 
   public async show({}: HttpContextContract) {}
