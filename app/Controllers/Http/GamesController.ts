cb6029a62feadf69f4f30029246ccf5e2551c6d0
João Gabriel <main.joaogabrielsg@gmail.com>
2022-01-17 14:03:27 -0300

    feat: add game validator and controller methods
    - Add game validator
    - Add game controller methods (store, update, destroy)

--- app/Controllers/Http/GamesController.ts
+++ app/Controllers/Http/GamesController.ts
@@ -1,5 +1,6 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Game from 'App/Models/Game'
+import GameValidator from 'App/Validators/GameValidator'
 
 export default class GamesController {
   public async index({}: HttpContextContract) {
@@ -9,7 +10,11 @@ export default class GamesController {
     return { min_cart_value: minCartValue, games }
   }
 
-  public async store({}: HttpContextContract) {}
+  public async store({ request }: HttpContextContract) {
+    const data = await request.validate(GameValidator)
+    const newGame = await Game.create(data)
+    return newGame
+  }
 
   public async show({ params, response }: HttpContextContract) {
     const { gameId } = params
@@ -22,7 +27,31 @@ export default class GamesController {
     }
   }
 
-  public async update({}: HttpContextContract) {}
+  public async update({ params, request, response }: HttpContextContract) {
+    const { gameId } = params
+    const data = await request.validate(GameValidator)
 
-  public async destroy({}: HttpContextContract) {}
+    try {
+      const game = await Game.findByOrFail('id', gameId)
+
+      game.merge(data)
+      await game.save()
+
+      return game
+    } catch (err) {
+      return response.status(err.status).json({ error: err.message })
+    }
+  }
+
+  public async destroy({ params, response }: HttpContextContract) {
+    const { gameId } = params
+
+    try {
+      const game = await Game.findByOrFail('id', gameId)
+      await game.delete()
+      return response.status(204)
+    } catch (err) {
+      return response.status(err.status).json({ error: err.message })
+    }
+  }
 }
