73fa9be7bbfc9e2d04aa4a31d174a9627ff33f37
João Gabriel <main.joaogabrielsg@gmail.com>
2022-01-18 13:32:09 -0300

    refactor: routes files to a unique route file

--- app/Modules/Auth/routes.ts
+++
@@ -1,5 +0,0 @@
-import Route from '@ioc:Adonis/Core/Route'
-
-Route.group(() => {
-  Route.post('/login', 'AuthController.login')
-}).prefix('v1')
--- start/routes.ts
+++ start/routes.ts
@@ -18,7 +18,46 @@
 |
 */
 
-import 'App/Modules/Auth/routes'
-import 'App/Modules/Bet/routes'
-import 'App/Modules/Game/routes'
-import 'App/Modules/User/routes'
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.group(() => {
+  Route.post('/login', 'AuthController.login')
+
+  Route.group(() => {
+    Route.get('/', 'BetsController.index')
+    Route.get('/:betId', 'BetsController.show')
+    Route.post('/', 'BetsController.store')
+  })
+    .prefix('/bets')
+    .where('id', /^[0-9]+$/)
+    .middleware(['auth'])
+
+  Route.group(() => {
+    Route.group(() => {
+      Route.get('/', 'GamesController.index')
+      Route.get('/:gameId', 'GamesController.show')
+    })
+
+    Route.group(() => {
+      Route.post('/', 'GamesController.store')
+      Route.put('/:gameId', 'GamesController.update')
+      Route.delete('/:gameId', 'GamesController.destroy')
+    }).middleware(['auth'])
+  })
+    .prefix('/games')
+    .where('id', /^[0-9]+$/)
+
+  Route.group(() => {
+    Route.group(() => {
+      Route.post('/', 'UsersController.store')
+    })
+
+    Route.group(() => {
+      Route.get('/', 'UsersController.index')
+      Route.put('/', 'UsersController.update')
+      Route.delete('/', 'UsersController.destroy')
+    }).middleware(['auth'])
+  })
+    .prefix('/users')
+    .where('id', /^[0-9]+$/)
+}).prefix('v1')
